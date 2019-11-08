6ff4baf2b7c938e622e30a63c77fd42ccca559d4
Justin <justin@phazon.app>
2019-11-07 21:28:22 -0600

    test

--- helpers/postgres/postgres_test.go
+++ helpers/postgres/postgres_test.go
@@ -1,18 +1,17 @@
-package postgres
+package postgres_test
 
 import (
-	"github.com/phazon85/Ascent-WoW/helpers/postgres"
 	"log"
 	"testing"
 )
 
-func TestCheckActiveRaid(t *testing.T) {
-	raidGroupID := "125446049853603841"
-	raid, err := postgres.Actions.
+func TestLoadAvailableRaidGroups(t *testing.T) {
+	rgs, err := LoadAvailableRaidGroups()
 	if err != nil {
-		log.Printf("Error getting active raid")
+		log.Printf("error getting all raids")
 	}
-	if raid == nil {
-		log.Printf("Error empty raid slice")
+
+	if rgs != nil {
+		log.Printf("error getting raid groups")
 	}
 }
